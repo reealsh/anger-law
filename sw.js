a1228ccfc388b0a4cbce196f08c44eff221f9ecc
reealsh <167520193+reealsh@users.noreply.github.com>
2026-06-12 12:02:00 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = "anger-law-cache-v1";
+const CACHE = "anger-law-cache-v3";
 const FILES = ["./", "./index.html", "./style.css", "./app.js", "./background.jpeg", "./manifest.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
